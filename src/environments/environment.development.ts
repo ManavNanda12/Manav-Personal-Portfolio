b27eac8f0bfed1e89e0b2821227bc990c6552bfd
ManavNanda12 <manavnanda2404@gmail.com>
2026-04-02 18:41:33 +0530

    fix: environment file add on

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: false,
+  emailjs: {
+    serviceId: 'service_bxpxf68',
+    contactUstemplateId: 'template_acrlk04',
+    replyBackTemplateId: 'template_fr1d6vo',
+    publicKey: 'yVaALXGA-VqlP3BZO'
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  emailjs: {
+    serviceId: 'service_bxpxf68',
+    contactUstemplateId: 'template_acrlk04',
+    replyBackTemplateId: 'template_fr1d6vo',
+    publicKey: 'yVaALXGA-VqlP3BZO'
+  }
+};
